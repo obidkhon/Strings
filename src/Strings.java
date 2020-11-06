90471670bc755b5a77cc1da6d7da34de4c355d79
obidkon <obid.sharipov.80@mail.ru>
2020-11-06 22:16:52 +0500

    this is Sitring

---
+++ src/Strings.java
@@ -0,0 +1,20 @@
+public class Strings {
+    public static void main(String[] args) {
+        System.out.println("Madlibs");
+        String usernam = "obid";
+        System.out.println("my _ sister accidentlly _ed all our her new _");
+
+
+
+        String adjectve= "hast" ;
+
+        String vorb = "run";
+          String noun ="program";
+
+        System.out.println("my " +adjectve+" sister accidentlly "+vorb+"ed all our her new"+noun+". ");
+
+
+
+    }
+
+}
